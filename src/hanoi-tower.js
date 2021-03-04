3f4fff5e3e3a60d0710f26424d2be8651c9ceec4
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-04 15:09:52 +0300

    feat: add implementation

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,11 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = Math.pow(2, disksNumber) - 1;
-  let speed = (turnsSpeed / 3600).toFixed(15);
-  let seconds =  turns / speed;
-  return {
-    turns: turns,
-    seconds: seconds,
-  }
+  let turns = Math.pow(2, disksNumber);
+  let res = {};
+  res.turns = turns;
+  res.seconds = turns / (turnsSpeed / 3600);
+  return res;
 };
