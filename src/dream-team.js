a40f92d03ef106550a235d8366097f069989c709
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-04 14:36:04 +0300

    feat: add implementation for some functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let str = '';
+  let arr = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') {
+      continue;
+    }
+    arr.push(members[i].trim());
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toUpperCase();
+  }
+
+  arr = arr.sort();
+
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i][0].toUpperCase();
+  }
+  return str;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  var array = [];
-  for(let i = 0; i < arr.length - 1; i++){
-    if (arr[i] === '--discard-prev'){
-      array.splice(i-1, i-1);
-    }
-    else if(arr[i] === '--discard-next'){
-      i++;
-    }
-    else if(arr[i] === '--double-next'){
-      array.push(arr[i+1]);
-    }
-    else if(arr[i] === '--double-next'){
-      array.push(arr[i-1]);
-    }
-    else{
-      array.push(arr[i]);
+  if (arr === null || arr === []) return [];
+
+  if (!Array.isArray(arr)) throw new Error('Not array');
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+
+    switch (item) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (result.length !== 0 && arr[i - 2] !== '--discard-next') {
+          result.pop();
+        }
+        break;
+      case '--double-next':
+        if (i + 1 < arr.length) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i - 1 > -1 && arr[i - 2] !== '--discard-next') {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(item);
     }
   }
-  return array;
+
+  return result;
 };
